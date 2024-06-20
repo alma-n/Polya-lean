6362b7d968bcd1e9e1e0ebffefda1865e40c84c9
Alma <166118464+alma-n@users.noreply.github.com>
2024-06-20 16:38:46 +0000

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,51 @@
+          
+{
+  description = "Constructive Logic and Formal Proof High-school Course";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    devenv.url = "github:cachix/devenv";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ inputs.devenv.flakeModule ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        let
+          lib = pkgs.lib;
+        in
+        {
+          # Per-system attributes can be defined here. The self' and inputs'
+          # module parameters provide easy access to attributes of the same
+          # system.
+
+          devenv.shells.default = {
+            languages.python = {
+              enable = true;
+              venv.enable = true;
+            };
+          };
+        };
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
+      };
+    };
+}
