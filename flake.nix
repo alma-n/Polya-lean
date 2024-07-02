a692ec84ff61169e29110fb59e64278ec926ef40
Niklas Halonen <niklas.halonen@protonmail.com>
2024-07-02 13:51:02 +0300

    add leanblueprint to flake.nix

--- flake.nix
+++ flake.nix
@@ -1,4 +1,3 @@
-          
 {
   description = "Constructive Logic and Formal Proof High-school Course";
 
@@ -29,6 +28,75 @@
         }:
         let
           lib = pkgs.lib;
+          pythonOverrides = [
+            (final: prev: {
+              plasTeX = pkgs.python3Packages.buildPythonPackage rec {
+                pname = "plasTeX";
+                version = "3.1";
+                src = pkgs.fetchPypi {
+                  inherit pname version;
+                  hash = "sha256-ai7n+8plYdP0y9ef4/SRi5BUdXOCxUIrn9oyTWRqt30=";
+                };
+
+                propagatedBuildInputs = with final; [
+                  typing-extensions
+                  pillow
+                  jinja2
+                  unidecode
+                ];
+
+                doCheck = false; # TODO
+              };
+
+              plastexdepgraph = pkgs.python3Packages.buildPythonPackage rec {
+                pname = "plastexdepgraph";
+                version = "0.0.4";
+                src = pkgs.fetchPypi {
+                  inherit pname version;
+                  hash = "sha256-LUw76Kn80lbGG67uTBWaQQfpjnSkUfCoD2wvcFv41L0=";
+                };
+
+                propagatedBuildInputs = with final; [
+                  pygraphviz
+                  plasTeX
+                ];
+              };
+
+              plastexshowmore = pkgs.python3Packages.buildPythonPackage rec {
+                pname = "plastexshowmore";
+                version = "0.0.2";
+                src = pkgs.fetchPypi {
+                  inherit pname version;
+                  hash = "sha256-8f6nIl6ufivT+RI1w2ySMQh+N5NZPIwTnDlVzQCPC1E=";
+                };
+
+                propagatedBuildInputs = with final; [ plasTeX ];
+              };
+
+              leanblueprint = pkgs.python3Packages.buildPythonPackage rec {
+                pname = "leanblueprint";
+                version = "0.0.10";
+                src = pkgs.fetchPypi {
+                  inherit pname version;
+                  hash = "sha256-z514yxhEcJwVIKae38cEkEZgyC4NoxCbokhGgBPQZvc=";
+                };
+                propagatedBuildInputs = with final; [
+                  plasTeX
+                  plastexshowmore
+                  plastexdepgraph
+                  click
+                  rich
+                  rich-click
+                  tomlkit
+                  jinja2
+                  GitPython
+                ];
+
+                doCheck = false;
+              };
+            })
+          ];
+          python = pkgs.python3.override { packageOverrides = lib.composeManyExtensions pythonOverrides; };
         in
         {
           # Per-system attributes can be defined here. The self' and inputs'
@@ -36,9 +104,11 @@
           # system.
 
           devenv.shells.default = {
+            packages = [ (python.withPackages (p: [ p.leanblueprint ])) ];
             languages.python = {
+              package = python;
               enable = true;
-              venv.enable = true;
+              # venv.enable = true;
             };
           };
         };
