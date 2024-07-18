905766c4e6dd0c353fa7334b38e60b4417a3e4ae
Niklas Halonen <niklas.halonen@protonmail.com>
2024-07-18 22:46:22 +0300

    remove old Lean code

--- Polya.lean
+++ Polya.lean
@@ -1,5 +1,4 @@
--- This module serves as the root of the `«Pólya»` library.
+-- This module serves as the root of the `«Polya»` library.
 -- Import modules here that should be built as part of the library.
-import «Polya».Basic
 import «Polya».MiscLemmas
 import «Polya».RegularizedOccupation
